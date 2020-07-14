517aff05ca2bcfc0dc69a17ce252d9c5740a7bb4
Kendra Ryan <yesthisiskendra@gmail.com>
2020-07-13 18:11:04 -0700

    Architecture

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import MovieApp from "./MovieApp";
 
 function App() {
   return (
     <div className="App">
-      <h1>PARTY TIME</h1>
+      <MovieApp />
     </div>
   );
 }
---
+++ src/components/MovieApp.js
@@ -0,0 +1,35 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+import MovieList from "./MovieList";
+
+const MovieApp = () => {
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "fafafa",
+      }}
+      elevation={0}
+    >
+      <AppBar color="primary" position="static" style={{ height: "64px" }}>
+        <Toolbar>
+          <Typography color="inherit">MOVIES FROM 2019</Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container justify="center" style={{ marginTop: "1rem" }}>
+        <Grid item xs={11} md={8} lg={4}>
+          <MovieList />
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
+
+export default MovieApp;
